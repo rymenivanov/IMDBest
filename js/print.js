e2165259674cd9b754145a49fe513fe85de84123
konstantinboev <konstantin.boev@mail.bg>
2017-10-07 18:36:45 +0300

    Added verification for the recently watched list

--- js/print.js
+++ js/print.js
@@ -1,108 +1,119 @@
 document.addEventListener('DOMContentLoaded', function () {
     /////////print movies////////////////////////////////
 
-    // var movie = [];
+    var recentlyWatched = [];
     var index = 0
     var templateMovie = document.getElementById('movieDIv').innerHTML;
     var searchEnter = document.getElementById("search-enter").addEventListener("click", function (event) {
         event.preventDefault()
-    // document.getElementById('printHere').innerHTML= ""
-        
+        // document.getElementById('printHere').innerHTML= ""
+
         // movie.push(getMovieInfo(document.querySelector("#srch-term").value))
         getMovieInfo(document.querySelector("#srch-term").value).then(function () {
             document.querySelector("#srch-term").value = ""
             var allMovies = {
                 news: [
-                    {   image:arrayMovies[index].Poster, 
-                        title:arrayMovies[index].Title,
-                        year:arrayMovies[index].Year,
-                        rated:arrayMovies[index].Rated,
-                        released:arrayMovies[index].Released,
-                        runtime:arrayMovies[index].Runtime,
-                        genre:arrayMovies[index].Genre,
-                        director:arrayMovies[index].Director,
-                        writer:arrayMovies[index].Writer,                                
-                        actors:arrayMovies[index].Actors,                                
-                        plot:arrayMovies[index].Plot,                  
-                        language:arrayMovies[index].Language,
-                        country:arrayMovies[index].Country,
-                        awards:arrayMovies[index].Awards,
-                        text:arrayMovies[index].Title,
-                        boxoffice:arrayMovies[index].BoxOffice,}
+                    {
+                        image: arrayMovies[index].Poster,
+                        title: arrayMovies[index].Title,
+                        year: arrayMovies[index].Year,
+                        rated: arrayMovies[index].Rated,
+                        released: arrayMovies[index].Released,
+                        runtime: arrayMovies[index].Runtime,
+                        genre: arrayMovies[index].Genre,
+                        director: arrayMovies[index].Director,
+                        writer: arrayMovies[index].Writer,
+                        actors: arrayMovies[index].Actors,
+                        plot: arrayMovies[index].Plot,
+                        language: arrayMovies[index].Language,
+                        country: arrayMovies[index].Country,
+                        awards: arrayMovies[index].Awards,
+                        text: arrayMovies[index].Title,
+                        boxoffice: arrayMovies[index].BoxOffice,
+                    }
                 ]
             }
+            var povtarqSe = false
+            recentlyWatched.forEach(function (element) {
+                if (element == arrayMovies[index].Title) {
+                    povtarqSe = true
+                }
+            })
 
-            var div = document.createElement("div")
-            div.className = "col-sm-12 aside"
-            var divImg = document.createElement("div")
-            var img = document.createElement("img")
-            img.src = arrayMovies[index].Poster
-            var divTitle = document.createElement("div")
-            var a = document.createElement("a")
-            // a.className = "printMovie"
-            // a.setAttribute("onclick","printMovie()")
-            a.innerText = arrayMovies[index].Title
-            divTitle.appendChild(a)
-            divImg.appendChild(img)
-            div.appendChild(divImg)
-            div.appendChild(divTitle)
-            document.querySelector(".aside").appendChild(div)
-            buton = true
-            
-                
-            
+            if (povtarqSe == false) {
+                var div = document.createElement("div")
+                div.className = "col-sm-12 aside"
+                var divImg = document.createElement("div")
+                var img = document.createElement("img")
+                img.src = arrayMovies[index].Poster
+                var divTitle = document.createElement("div")
+                var a = document.createElement("a")
+                // a.className = "printMovie"
+                // a.setAttribute("onclick","printMovie()")
+                a.innerText = arrayMovies[index].Title
+                divTitle.appendChild(a)
+                divImg.appendChild(img)
+                div.appendChild(divImg)
+                div.appendChild(divTitle)
+                document.querySelector(".aside").appendChild(div)
+                recentlyWatched.push(arrayMovies[index].Title)
+                // buton = true
+            }
+
+
+
 
             index++
             var template = Handlebars.compile(templateMovie);
             var readyHTML = template(allMovies);
             document.getElementById('printHere').innerHTML = readyHTML;
-        
+
         })
     })
     // var printMovie= function() {
-        // console.log("asd")
-        // var film = arrayMovies.find(function(movie) {
-        //     movie.Title == searchlink.innerText
-        // })
-        // console.log(film)
+    // console.log("asd")
+    // var film = arrayMovies.find(function(movie) {
+    //     movie.Title == searchlink.innerText
+    // })
+    // console.log(film)
     // }
 
 
     /////////print movies/////////////////////////////////
     /////////userNamelogin////////////////////////////////
-    document.getElementById("signIN").addEventListener("click",function () {
+    document.getElementById("signIN").addEventListener("click", function () {
         event.preventDefault();
         var userName = document.getElementById("inputLoginName").value
         var userPassword = document.getElementById("inputLoginPassword").value
 
-        if (userList.login(userName,userPassword)) {
-            var watchlist = document.getElementById("watchlist").style.display="block"
+        if (userList.login(userName, userPassword)) {
+            var watchlist = document.getElementById("watchlist").style.display = "block"
             // alert("BRAVO VLEZNA")
-            var toHide =document.getElementById("toHide").style.display="none"
-            var logout = document.getElementById("logout").style.display="block"
+            var toHide = document.getElementById("toHide").style.display = "none"
+            var logout = document.getElementById("logout").style.display = "block"
         }
     })
     /////////userNamelogin////////////////////////////////
     /////////userNameRegistration////////////////////////////////  
-    document.getElementById("joinBtn").addEventListener("click",function () {
+    document.getElementById("joinBtn").addEventListener("click", function () {
         event.preventDefault();
         var userNameRegister = document.getElementById("inputRegisterName").value
         var userPasswordRegister = document.getElementById("inputRegisterPassword").value
         var userEmailRegister = document.getElementById("inputRegisterEmail").value
 
-        if (userList.addUser(userNameRegister,userPasswordRegister,userEmailRegister)) {
-            userList.addUser(userNameRegister,userPasswordRegister,userEmailRegister)
+        if (userList.addUser(userNameRegister, userPasswordRegister, userEmailRegister)) {
+            userList.addUser(userNameRegister, userPasswordRegister, userEmailRegister)
             // alert("BRAVO VLEZNA")            
         }
     })
     /////////userNameRegistration////////////////////////////////  
     //////// logout//////////////////////////////////////////////
-    document.getElementById("logout").addEventListener("click",function () {
+    document.getElementById("logout").addEventListener("click", function () {
         location.reload();
     })
     // $('#something').click(function() {
     //     location.reload();
     // });
     /////////////logout//////////////
-    
+
 });
