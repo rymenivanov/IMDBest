2b755636be7c0d935938521837fcd139057f6a42
AntonioDim <antonio.dimitrov.94@gmail.com>
2017-10-10 01:48:18 +0300

    added things... movies prints etc

--- js/userController.js
+++ js/userController.js
@@ -1,9 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // document.querySelector("#addToWatchlist").addEventListener("click", function () {
-    //     var loggedUser = JSON.parse(sessionStorage.getItem("Loged user"))
-    //     if (loggedUser != -1)
-    //         loggedUser.arrayWatchList.push(arrayMovies[arrayMovies.length-1])
-    //         console.log(loggedUser.arrayWatchList)
-    //         console.log(loggedUser)
-    // })
-})
+
