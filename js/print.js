9e2ac7c0ec2509058da605603b957a0808ae2902
konstantinboev <konstantin.boev@mail.bg>
2017-10-10 03:39:53 +0300

    Fixed watchlist delete button

--- js/print.js
+++ js/print.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     /////////print movies////////////////////////////////
-    
+
     function searchAgainEvent(id) {
         document.getElementById(id).addEventListener("click", function (e) {
             arrayMovies.forEach(function (element) {
@@ -10,6 +10,17 @@ document.addEventListener('DOMContentLoaded', function () {
                 }
             })
         })
+    }
+
+    function updateStorages(user) {
+        sessionStorage.setItem("Loged user", JSON.stringify(user))
+        var local = JSON.parse(localStorage.getItem("users"))
+        local.forEach(function (element) {
+            if (element.username == user.username) {
+                element.arrayWatchList = user.arrayWatchList.slice(0)
+            }
+        })
+        localStorage.setItem("users", JSON.stringify(local))
     }
 
     var liIndex = 0
@@ -54,9 +65,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
             if (povtarqSe == false) {
                 var spanID = "btnRecentlyViewed" + index
-                $(".aside").append('<div class="col-sm-12 asideDivs" style="height: 65px;"><div><img src="'+arrayMovies[index].Poster+'"></div><span id="'+spanID+'"><a>'+arrayMovies[index].Title+'</a></span></div></div>')
+                $(".aside").append('<div class="col-sm-12 asideDivs" style="height: 65px;"><div><img src="' + arrayMovies[index].Poster + '"></div><span id="' + spanID + '"><a>' + arrayMovies[index].Title + '</a></span></div></div>')
                 recentlyWatched.push(arrayMovies[index].Title)
-                searchAgainEvent(spanID)  
+                searchAgainEvent(spanID)
             }
 
             index++
@@ -66,47 +77,72 @@ document.addEventListener('DOMContentLoaded', function () {
 
         }).then(function () {
             $("#buttonForWatch").append('<button id="addToWatchlist" style="position:fixed;bottom:0;background-color: rgb(52, 52, 52);"class="btn btn-primary btn-block">Add to Watchlist</button>')
-            $("#buttonForWatch").append('<button id="deleteFromWatchlist" style="display:none;position:fixed;bottom:35px;background-color: rgb(52, 52, 52);"class="btn btn-primary btn-block"><img style="height:20px;" src="images/trash_can.png" /></button>')
+            var loggedUser = JSON.parse(sessionStorage.getItem("Loged user"))
+            if (loggedUser != null) {
+                $("#buttonForWatch").append('<button id="deleteFromWatchlist" style="position:fixed;bottom:35px;background-color: rgb(52, 52, 52);"class="btn btn-primary btn-block"><img style="height:20px;" src="images/trash_can.png" /></button>')
+                $("#deleteFromWatchlist").on("click", function () {
+                    loggedUser.arrayWatchList.forEach(function (element) {
+                        if (element.Title == arrayMovies[arrayMovies.length - 1].Title) {
+                            var indexToDelete = loggedUser.arrayWatchList.indexOf(element)
+                            loggedUser.arrayWatchList.splice(indexToDelete, 1)
+                            console.log(loggedUser.arrayWatchList)
+                        }
+                    })
+                    console.log(loggedUser)
+                    updateStorages(loggedUser)
+                })
+            }
             $("#addToWatchlist").on("click", function () {
                 var loggedUser = JSON.parse(sessionStorage.getItem("Loged user"))
                 povtarqSe = false
-                if (loggedUser != -1) {
+                if (loggedUser != null) {
 
-                if (loggedUser.arrayWatchList.length == 0) {
-                    printWatchlist(arrayMovies[arrayMovies.length - 1])
-                } else {
+                    if (loggedUser.arrayWatchList.length == 0) {
+                        loggedUser.arrayWatchList.push(arrayMovies[arrayMovies.length - 1])
+                    } else {
 
-                    loggedUser.arrayWatchList.forEach(function (element) {
-                        if (element.Title == arrayMovies[arrayMovies.length - 1].Title) {
-                            povtarqSe = true
+                        loggedUser.arrayWatchList.forEach(function (element) {
+                            if (element.Title == arrayMovies[arrayMovies.length - 1].Title) {
+                                povtarqSe = true
+                            }
+                        })
+
+                        if (povtarqSe == false) {
+                            loggedUser.arrayWatchList.push(arrayMovies[arrayMovies.length - 1])
                         }
-                    })
-
-                    if (povtarqSe == false) {
-                        printWatchlist(arrayMovies[arrayMovies.length - 1])
                     }
+
+                    updateStorages(loggedUser)
+
+
+                    // function printWatchlist(movie) {
+                    //     var liID = "btnWatchlistLi" + liIndex
+                    //     $("#watchlistDropdown").append("<li id='"+liID+"'><a href='#'>" + movie.Title + "</a></li>")
+                    //     searchAgainEvent(liID)
+                    //     liIndex++
+                    // }
+
                 }
-                sessionStorage.setItem("Loged user", JSON.stringify(loggedUser))
-                var local = JSON.parse(localStorage.getItem("users"))
-                local.forEach(function (element) {
-                    if (element.username == loggedUser.username) {
-                        element.arrayWatchList = loggedUser.arrayWatchList.slice(0)
-                    }
-                })
-                localStorage.setItem("users", JSON.stringify(local))
-
-
-                function printWatchlist(movie) {
-                    var liID = "btnWatchlistLi" + liIndex
-                    $("#watchlistDropdown").append("<li id='"+liID+"'><a href='#'>" + movie.Title + "</a></li>")
-                    searchAgainEvent(liID)
-                    liIndex++
-                    loggedUser.arrayWatchList.push(movie)
-                }
-
-            }
 
             })
+            document.querySelector("#watchlist").addEventListener("click", function () {
+                var loggedUser = JSON.parse(sessionStorage.getItem("Loged user"))
+                var local = JSON.parse(localStorage.getItem("users"))
+                $("#watchlistDropdown").empty()
+                local.forEach(function (element) {
+                    if (element.username == loggedUser.username) {
+                        element.arrayWatchList.forEach(function (el) {
+                            var liID = "btnWatchlistLi" + liIndex
+                            $("#watchlistDropdown").append("<li id='" + liID + "'><a href='#'>" + el.Title + "</a></li>")
+                            searchAgainEvent(liID)
+                            liIndex++
+                        })
+                    }
+                })
+            })
+
+
+
         })
     })
 
--- js/userController.js
+++ js/userController.js
@@ -1,9 +1,3 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // document.querySelector("#addToWatchlist").addEventListener("click", function () {
-    //     var loggedUser = JSON.parse(sessionStorage.getItem("Loged user"))
-    //     if (loggedUser != -1)
-    //         loggedUser.arrayWatchList.push(arrayMovies[arrayMovies.length-1])
-    //         console.log(loggedUser.arrayWatchList)
-    //         console.log(loggedUser)
-    // })
+
 })
